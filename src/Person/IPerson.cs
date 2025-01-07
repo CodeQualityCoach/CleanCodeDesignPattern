d2a0f968e81f808cbf1b8256536fcf550cc5fb3c
Onkel Mato <46560885+OnkelMato@users.noreply.github.com>
2025-01-07 15:03:08 +0100

    worst comment ever

---
+++ src/Person/IPerson.cs
@@ -0,0 +1,14 @@
+﻿namespace Person
+{
+    public interface IHaveUniqueIdentifier
+    {
+        int Id { get; set; }
+    }
+
+    public interface IAmPerson
+         : IHaveUniqueIdentifier
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+    }
+}
--- src/Person/Person.cs
+++ src/Person/Person.cs
@@ -8,22 +8,73 @@
      * Interface Segregation Principle (ISP)
      ********************************************************/
 
+    public interface ICanPersist
+    {
+        string Load(string path);
+        void Save(string path, string serializedString);
+    }
 
-    public class Person
+    public class FilePersister : ICanPersist
+    {
+        public string Load(string path)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Save(string path, string serializedString)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public interface ICanConvertPerson
+    {
+        string ToString(Person person);
+        Person FromString(string json);
+    }
+
+    public class JsonPersonConverter : ICanConvertPerson
+    {
+        public Person FromString(string json)
+        {
+            // konvertiert json nach person
+            var x = new Person();
+            return x;
+        }
+
+        public string ToString(Person person)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class PersonFactory
+    {
+        private readonly ICanPersist persister;
+        private readonly ICanConvertPerson personConverter;
+
+        public PersonFactory(ICanPersist persister, ICanConvertPerson personConverter)
+        {
+            this.persister = persister;
+            this.personConverter = personConverter;
+        }
+
+
+        // somewhere datei oder www, somewhat xml oder json
+        public Person LoadFromSomewhereAsSomewhat(string persistentLocationPathOrWWW)
+        {
+            var srcString = persister.Load(persistentLocationPathOrWWW);
+            var result = personConverter.FromString(srcString);
+
+            return result;
+        }
+    }
+
+    public class Person : IAmPerson
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-
-        public void Load(string path)
-        {
-            // Lesen der Daten aus der Datei...
-        }
-
-        public void Save(string path)
-        {
-            // Speichern der Daten in die Datei...
-        }
 
         public string ToJsonString()
         {
@@ -37,6 +88,18 @@
             // Zurückliefern der Daten als XML-Zeichenkette...
             var xmlString = "...";
             return xmlString;
+        }
+    }
+
+    public class Prog
+    {
+        public void Main()
+        {
+            // laden einer person
+            //var factory = new PersonFactory(new FilePersister(), new JsonPersonConverter());
+            var factory = new PersonFactory(new DatabasePersister(), new XmlPersonConverter());
+            var person =factory.LoadFromSomewhereAsSomewhat("c:/foo.json");
+
         }
     }
 }
